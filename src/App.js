8ba392abc3dc16f312e02ef5df182ec70558f99b
Tamy <tamy8677@gmail.com>
2024-12-01 13:36:06 +0800

    以函式實現React component，並模組化

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button>修改後App函式</button>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
-export default App;
+export default App; // 將函式輸出 給其他檔案（模組）引入
+// 把輸出的東西包成一個「物件」讓別人存取，輸入檔就能不用{}去取單一的東西，方法是加上default關鍵字、包成一個物件後，把要一起export的東西都丟進去這個物件
--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
 import React from 'react'; // 引入用npm載下來的React
 import ReactDOM from 'react-dom/client'; // 引入用npm載下來的ReactDOM
 import './index.css';
-import App from './App';
+import App from './App'; // 從App.js檔案（模組） 引入App()函式
+// 引入的函式（被包成物件，所以不需要{}）要被作為React component使用，所以還是要以import React from 'react';引入React
 import reportWebVitals from './reportWebVitals';
 
 // 而React特別的地方在，當我們寫React時，是對React自己的DOM操作
 // React DOM函式`ReactDOM.render()`是React的程式進入點，正確的說法是React的 virtual DOM 。
 // React不是像過去我們操作DOM時一樣「一個指令一個動作」，而是先用js做出一個模擬的DOM，當畫面產生改變，React會把更改的地方放入virtual DOM，和原本的DOM比較後，再去更改DOM上「必須更改的地方」。
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Hello(){
+  return(
+    <button>大家好</button>
+  );
+};
+
+function Progress(){
+  const barWidth="50%";
+  return(
+    <div>
+      <div className="progress-back" style={{backgroundColor:"rgba(0,0,0,0.2)",width:"200px",height:"7px",borderRadius:"10px"}}>
+        <div className="progress-bar" style={{backgroundColor:"#fe5196",width:barWidth,height:"100%",borderRadius:"10px"}}></div>
+      </div>
+    </div>
+  );
+}
+
 root.render( // 呼叫一個物件「ReactDOM」的render函式
 // 此函式接收兩個參數:
 // 第一個參數`<h1> Hello world!</h1>`是我們所渲染到畫面上的內容。
 // 第二個參數`document.getElementById('root')`是React所綁定的原始div元素。
-  <h1>Hello world!</h1>,
+  <div>
+    <Hello/>
+    <Hello/>
+    <Hello/>
+    <Progress/>
+    <App/>
+  </div>, 
+  // Hello 函式 成為了 component
+  // 一般JSX 寫成 {函式名稱}
+  // JSX當中的React component語法 寫成 <函式名稱/>
+  // App函式是從其他檔案（模組）引入的
   <React.StrictMode>
     <App />
   </React.StrictMode>
